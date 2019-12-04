c888a3313266b53e3a1d17af1717f701e788d776
sysbiocoder <subazinitk@gmail.com>
2019-12-04 16:27:35 +0100

    Update Qualityasses.R

--- Qualityasses.R
+++ Qualityasses.R
@@ -1,8 +1,11 @@
 # Density Plot
 data <- read.table("countfile.txt",sep="\t",header=T)
+cnt_tf <- log(data[,2],10)  
+d <- density(cnt_tf)  
+plot(d,xlab="Count (log10)", ylab="Density")
 pdf(file="Countdensity.pdf")  
 
-for (cnt in 2:length(data))  #count columns
+for (cnt in 3:length(data))  #count columns
 {  
 
 cnt_tf <- log(data[,cnt],10)  
